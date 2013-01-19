8a5db0714dd9e9fae300dc825db2670b310055f7
Denis Knauf <Denis.Knauf@gmail.com>
2013-01-19 21:13:15 +0100

    ace: uniq generating files. SassFilter: sass possible too; instead substitude extention, .scss|.sass will be removed

--- lib/ace/filters/sass.rb
+++ lib/ace/filters/sass.rb
@@ -6,15 +6,18 @@ require "ace/filters"
 module Ace
   class SassFilter < Filter
     def call(item, content)
-      if item.output_path && item.output_path.end_with?(".scss")
-        item.output_path.sub!(/scss$/, "css")
+      if item.output_path && item.output_path.match(/\.s[ac]ss$/)
+        syntax = item.output_path.end_with?(".scss") ? :scss : :sass
+        item.output_path.sub!(/\.s[ac]ss$/, '')
+        output = nil
         begin
-          engine = Sass::Engine.new(content, {:syntax => :scss})
+          engine = Sass::Engine.new(content, syntax: syntax, filename: item.original_path)
           output = engine.render
         rescue Exception => e
           warn "~~ SassFilter exception: #{e}"
           abort
         end
+        #p caller: Kernel.caller
         return output
       else
         return content
